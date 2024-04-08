f65df41952999e3c304c03258b0ff9fce2de0039
oystebw <oystebw@stud.ntnu.no>
2024-04-08 21:36:26 +0200

    threads

--- src/image_processing_opencl.cpp
+++ src/image_processing_opencl.cpp
@@ -13,6 +13,7 @@
 extern "C"{
     #include "ppm.h"
 }
+#include <omp.h>
 
 
 using namespace std;
@@ -273,6 +274,7 @@ int main(int argc, char** argv){
     OpenClBlur blur;
     const int sizes[] = {2, 3, 5, 8};
     AccurateImage* images[4];
+    #pragma omp parallel for num_threads(4)
     for(int i = 0; i < 4; i++){
         images[i] = blur.blur(imageAccurate, sizes[i]);
     }
