6d8e5804fea6bffa04813b1164513f88fad600cd
oystebw <oystebw@stud.ntnu.no>
2024-04-08 16:38:35 +0200

    lets try

--- src/image_processing_c.c
+++ src/image_processing_c.c
@@ -25,19 +25,19 @@ typedef struct {
 
 typedef struct {
      int x, y;
-     float16x4_t* data;
+     float32x4_t* data;
 } AccurateImage;
 
 AccurateImage* convertToAccurateImage(const PPMImage* image) {
 	const int size = image->x * image->y;
 
 	AccurateImage* imageAccurate = (AccurateImage*)malloc(sizeof(AccurateImage));
-	imageAccurate->data = (v4Accurate*)malloc(size * sizeof(v4Accurate));
+	imageAccurate->data = (float32x4_t*)malloc(size * sizeof(float32x4_t));
 	#pragma GCC unroll 8
 	for(int i = 0; i < size; i++) {
-		imageAccurate->data[i][0] = (float) image->data[i].red;
-		imageAccurate->data[i][1] = (float) image->data[i].green;
-		imageAccurate->data[i][2] = (float) image->data[i].blue;
+		imageAccurate->data[i].val[0] = (float) image->data[i].red;
+		imageAccurate->data[i].val[1] = (float) image->data[i].green;
+		imageAccurate->data[i].val[2] = (float) image->data[i].blue;
 	}
 	imageAccurate->x = image->x;
 	imageAccurate->y = image->y;
@@ -48,9 +48,9 @@ AccurateImage* convertToAccurateImage(const PPMImage* image) {
 AccurateImage* copyAccurateImage(const AccurateImage* imageIn) {
 	const int size = imageIn->x * imageIn->y;
 	AccurateImage* imageOut = (AccurateImage*)malloc(sizeof(AccurateImage));
-	imageOut->data = (v4Accurate*)malloc(size * sizeof(v4Accurate));
+	imageOut->data = (float32x4_t*)malloc(size * sizeof(float32x4_t));
 	
-	memcpy(imageOut->data, imageIn->data, size * sizeof(v4Accurate));
+	memcpy(imageOut->data, imageIn->data, size * sizeof(float32x4_t));
 
 	imageOut->x = imageIn->x;
 	imageOut->y = imageIn->y;
@@ -67,9 +67,9 @@ PPMImage* convertToPPPMImage(const AccurateImage* imageIn) {
     imageOut->y = imageIn->y;
 	#pragma GCC unroll 8
     for(int i = 0; i < size; i++) {
-		imageOut->data[i].red = imageIn->data[i][0];
-		imageOut->data[i].green = imageIn->data[i][1];
-		imageOut->data[i].blue = imageIn->data[i][2];
+		imageOut->data[i].red = imageIn->data[i].val[0];
+		imageOut->data[i].green = imageIn->data[i].val[1];
+		imageOut->data[i].blue = imageIn->data[i].val[2];
     }
     return imageOut;
 }
@@ -77,71 +77,70 @@ PPMImage* convertToPPPMImage(const AccurateImage* imageIn) {
 void blurIteration(AccurateImage* image, const int size) {
 	const int width = image->x;
 	const int height = image->y;
-	v4Accurate* scratch = (v4Accurate*)malloc(width * height * sizeof(v4Accurate));
+	float32x4_t* scratch = (float32x4_t*)malloc(width * height * sizeof(float32x4_t));
 
 	// Transpose to be more cache / access friendly
-	v4Accurate (*data)   [width] = (void*) image->data;
-  	v4Accurate (*buffer) [height] = (void*) scratch;
+	float32x4_t (*data)   [width] = (void*) image->data;
+  	float32x4_t (*buffer) [height] = (void*) scratch;
 	
 	#pragma GCC unroll 5
 	for(int i = 0; i < BLUR_ITERATIONS; i++) {
 		#pragma GCC unroll 8
 		for(int y = 0; y < height; y++) {
 
-			v4Accurate sum = {0.0, 0.0, 0.0, 0.0};
+			float32x4_t sum = {0.0, 0.0, 0.0, 0.0};
 
 			for(int x = 0; x <= size; x++) {
-				sum += data[y][x];
+				sum = vaddq_f32(sum, data[y][x]);
 			}
 
-			buffer[0][y] = sum / (v4Accurate){size + 1, size + 1, size + 1, size + 1};
+			buffer[0][y] = vmulq_f32(sum, vrecpeq_f32((float32x4_t){size + 1, size + 1, size + 1, size + 1}));
 
 			for(int x = 1; x <= size; x++) {
-				sum += data[y][x + size];
-				buffer[x][y] = sum / (v4Accurate){size + x + 1, size + x + 1, size + x + 1, size + x + 1};
+				sum = vaddq_f32(sum, data[y][x + size]);
+				buffer[x][y] = vmulq_f32(sum, vrecpeq_f32((float32x4_t){size + x + 1, size + x + 1, size + x + 1, size + x + 1}));
 			}
 
 			for(int x = size + 1; x < width - size; x++) {
-				sum -= data[y][x - size - 1];
-				sum += data[y][x + size];
-				buffer[x][y] = sum / (v4Accurate){2 * size + 1, 2 * size + 1, 2 * size + 1, 2 * size + 1};
+				sum = vaddq_f32(-sum, data[y][x - size - 1]);
+				sum = vaddq_f32(sum, data[y][x + size]);
+				buffer[x][y] = vmulq_f32(sum, vrecpeq_f32((float32x4_t){2 * size + 1, 2 * size + 1, 2 * size + 1, 2 * size + 1}));
 			}
 
 			for(int x = width - size; x < width; x++) {
-				sum -= data[y][x - size - 1];
-				buffer[x][y] = sum / (v4Accurate){size + width - x, size + width - x, size + width - x, size + width - x};
+				sum = vaddq_f32(-sum, data[y][x - size - 1]);
+				buffer[x][y] = vmulq_f32(sum, vrecpeq_f32((float32x4_t){size + width - x, size + width - x, size + width - x, size + width - x}));
 			}
 			
 		}
 		#pragma GCC unroll 8
 		for(int x = 0; x < width; x++) {
 
-			v4Accurate sum = {0.0, 0.0, 0.0, 0.0};
+			float32x4_t sum = {0.0, 0.0, 0.0, 0.0};
 
 			for(int y = 0; y <= size; y++) {
-				sum += buffer[x][y];
+				sum = vaddq_f32(sum, buffer[x][y]);
 			}
 
-			data[0][x] = sum / (v4Accurate){size + 1, size + 1, size + 1, size + 1};
+			data[0][x] = vmulq_f32(sum, vrecpeq_f32(float32x4_t){size + 1, size + 1, size + 1, size + 1}));
 
 			for(int y = 1; y <= size; y++) {
-				sum += buffer[x][(y + size)];
-				data[y][x] = sum / (v4Accurate){y + size + 1, y + size + 1, y + size + 1, y + size + 1};
+				sum = vaddq_f32(sum, buffer[x][(y + size)]);
+				data[y][x] = vmulq_f32(sum, vrecpeq_f32((float32x4_t){y + size + 1, y + size + 1, y + size + 1, y + size + 1}));
 			}
 
 			for(int y = size + 1; y < height - size; y++) {
-				sum -= buffer[x][y - size - 1];
-				sum += buffer[x][y + size];
-				data[y][x] = sum / (v4Accurate){2 * size + 1, 2 * size + 1, 2 * size + 1, 2 * size + 1};
+				sum = vaddq_f32(-sum, buffer[x][y - size - 1]);
+				sum = vaddq_f32(sum, buffer[x][y + size]);
+				data[y][x] = vmulq_f32(sum, vrecpeq_f32((float32x4_t){2 * size + 1, 2 * size + 1, 2 * size + 1, 2 * size + 1}));
 			}
 
 			for(int y = height - size; y < height; y++) {
-				sum -= buffer[x][y - size - 1];
-				data[y][x] = sum / (v4Accurate){size + height - y, size + height - y, size + height - y, size + height - y};
+				sum = vaddq_f32(-sum, buffer[x][y - size - 1]);
+				data[y][x] = vmulq_f32(sum, vrecpeq_f32((float32x4_t){size + height - y, size + height - y, size + height - y, size + height - y}));
 			}
 		}
 	}
-
 	free(scratch);
 }
 
@@ -157,10 +156,10 @@ PPMImage* imageDifference(const AccurateImage* imageInSmall, const AccurateImage
 	imageOut->y = height;
 	#pragma GCC unroll 8
 	for(int i = 0; i < size; i++) {
-		v4Accurate diffvec = imageInLarge->data[i] - imageInSmall->data[i];
-		float red = diffvec[0];
-		float green = diffvec[1];
-		float blue = diffvec[2];
+		float32x4_t diffvec = vaddq_f32(imageInLarge->data[i], -imageInSmall->data[i]);
+		float red = diffvec.val[0];
+		float green = diffvec.val[1];
+		float blue = diffvec.val[2];
 		red += 257.0 * (red < 0.0);
 		green += 257.0 * (green < 0.0);
 		blue += 257.0 * (blue < 0.0);
