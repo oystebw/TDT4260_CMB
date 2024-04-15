ce97c4bff74801b8d99ad2b912bf2e2dea98ac08
oystebw <oystebw@stud.ntnu.no>
2024-04-15 23:12:58 +0200

    different openmp

--- src/image_processing_c.c
+++ src/image_processing_c.c
@@ -13,70 +13,14 @@ typedef __uint32_t v4Int __attribute__((vector_size(16)));
 // http://7-themes.com/6971875-funny-flowers-pictures.html
 
 typedef struct {
-	unsigned char rgb[3];
-} v3Byte;
-
-typedef struct {
-	int x, y;
-	v3Byte* data;
-} SimpleImage;
-
-typedef struct {
      int x, y;
      v4Accurate* data;
 } AccurateImage;
 
-AccurateImage* convertToAccurateImage(const PPMImage* image) {
-	const int width = image->x;
-	const int height = image->y;
-	const int size = width * height;
-
-	AccurateImage* imageAccurate = (AccurateImage*)malloc(sizeof(AccurateImage));
-	imageAccurate->data = (v4Accurate*)malloc(size * sizeof(v4Accurate));
-	#pragma GCC unroll 8
-	for(int i = 0; i < size; i++) {
-		PPMPixel pixel = image->data[i];
-		imageAccurate->data[i][0] = (float) pixel.red;
-		imageAccurate->data[i][1] = (float) pixel.green;
-		imageAccurate->data[i][2] = (float) pixel.blue;
-	}
-	imageAccurate->x = width;
-	imageAccurate->y = height;
-	
-	return imageAccurate;
-}
-
-AccurateImage* copyAccurateImage(const AccurateImage* imageIn) {
-	const int size = imageIn->x * imageIn->y;
-	AccurateImage* imageOut = (AccurateImage*)malloc(sizeof(AccurateImage));
-	imageOut->data = (v4Accurate*)malloc(size * sizeof(v4Accurate));
-	
-	memcpy(imageOut->data, imageIn->data, size * sizeof(v4Accurate));
-
-	imageOut->x = imageIn->x;
-	imageOut->y = imageIn->y;
-	return imageOut;
-}
-
-PPMImage* convertToPPPMImage(const AccurateImage* imageIn) {
-    const int size = imageIn->x * imageIn->y;
-	
-	PPMImage* imageOut = (PPMImage*)malloc(sizeof(PPMImage));
-    imageOut->data = (PPMPixel*)malloc(size * sizeof(PPMPixel));
-
-    imageOut->x = imageIn->x;
-    imageOut->y = imageIn->y;
-	#pragma GCC unroll 8
-    for(int i = 0; i < size; i++) {
-		imageOut->data[i].red = imageIn->data[i][0];
-		imageOut->data[i].green = imageIn->data[i][1];
-		imageOut->data[i].blue = imageIn->data[i][2];
-    }
-    return imageOut;
-}
 
 void blurIterationHorizontalFirst(PPMPixel* in, v4Accurate* out, const int size, const int width, const int height) {
 	
+	#pragma omp parallel for simd num_threads(8)
 	for(int y = 0; y < height; y++) {
 		const int yWidth = y * width;
 
@@ -113,6 +57,7 @@ void blurIterationHorizontalFirst(PPMPixel* in, v4Accurate* out, const int size,
 
 void blurIterationHorizontal(v4Accurate* in, v4Accurate* out, const int size, const int width, const int height) {
 	
+	#pragma omp parallel for simd num_threads(8)
 	for(int y = 0; y < height; y++) {
 		const int yWidth = y * width;
 
@@ -144,6 +89,7 @@ void blurIterationHorizontal(v4Accurate* in, v4Accurate* out, const int size, co
 
 void blurIterationHorizontalTranspose(v4Accurate* in, v4Accurate* out, const int size, const int width, const int height) {
 	
+	#pragma omp parallel for simd num_threads(8)
 	for(int y = 0; y < height; y++) {
 		const int yWidth = y * width;
 
@@ -175,6 +121,7 @@ void blurIterationHorizontalTranspose(v4Accurate* in, v4Accurate* out, const int
 
 void blurIterationVertical(v4Accurate* in, v4Accurate* out, const int size, const int width, const int height) {
 
+	#pragma omp parallel for simd num_threads(8)
 	for(int x = 0; x < width; x++) {
 		const int xHeight = x * height;
 
@@ -204,36 +151,6 @@ void blurIterationVertical(v4Accurate* in, v4Accurate* out, const int size, cons
 	}
 }
 
-void blurIterationVerticalTranspose(v4Accurate* in, v4Accurate* out, const int size, const int width, const int height) {
-
-	for(int x = 0; x < width; x++) {
-		const int xHeight = x * height;
-
-		v4Accurate sum = {0.0, 0.0, 0.0, 0.0};
-
-		for(int y = 0; y <= size; y++) {
-			sum += in[xHeight + y];
-		}
-
-		out[0 * width + x] = sum / (v4Accurate){size + 1, size + 1, size + 1, size + 1};
-
-		for(int y = 1; y <= size; y++) {
-			sum += in[xHeight + y + size];
-			out[y * width + x] = sum / (v4Accurate){y + size + 1, y + size + 1, y + size + 1, y + size + 1};
-		}
-
-		for(int y = size + 1; y < height - size; y++) {
-			sum -= in[xHeight + y - size - 1];
-			sum += in[xHeight + y + size];
-			out[y * width + x] = sum / (v4Accurate){2 * size + 1, 2 * size + 1, 2 * size + 1, 2 * size + 1};
-		}
-
-		for(int y = height - size; y < height; y++) {
-			sum -= in[xHeight + y - size - 1];
-			out[y * width + x] = sum / (v4Accurate){size + height - y, size + height - y, size + height - y, size + height - y};
-		}
-	}
-}
 
 PPMImage* imageDifference(const AccurateImage* imageInSmall, const AccurateImage* imageInLarge) {
 	const int width = imageInSmall->x;
@@ -289,7 +206,6 @@ int main(int argc, char** argv) {
 		images[i]->data = (v4Accurate*)malloc(size * sizeof(v4Accurate));
 	}
 
-	#pragma omp parallel for simd num_threads(4)
 	for(int i = 0; i < 4; i++) {
 		blurIterationHorizontalFirst(image->data, scratches + i * size, sizes[i], width, height);
 		blurIterationHorizontal(scratches + i * size, images[i]->data, sizes[i], width, height);
