005186b04bc3b7cf29a8498211054125eb687a19
oystebw <oystebw@stud.ntnu.no>
2024-04-17 00:56:52 +0200

    pics

--- src/image_processing_c.c
+++ src/image_processing_c.c
@@ -138,7 +138,7 @@ void blurIterationVertical(v4Accurate* restrict in, v4Accurate* restrict out, co
 		const int xHeight = x * height;
 		
 		int factor;
-		if(x > size && x <= width - size) {
+		if(x >= size && x < width - size) {
 			factor = 2 * size + 1;
 		} 
 		else if(x < size) {
@@ -147,6 +147,7 @@ void blurIterationVertical(v4Accurate* restrict in, v4Accurate* restrict out, co
 		else {
 			factor = size + width - x;
 		}
+		factor = 2 * size + 1;
 
 		for(int iteration = 0; iteration < 5; iteration++) {
 			
