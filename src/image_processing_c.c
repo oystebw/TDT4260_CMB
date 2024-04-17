44941a56c8064a0fce7265bf00cb3f1357f81d75
oystebw <oystebw@stud.ntnu.no>
2024-04-17 02:44:56 +0200

    preinc

--- src/image_processing_c.c
+++ src/image_processing_c.c
@@ -22,25 +22,25 @@ typedef struct {
 void blurIterationHorizontalFirst(const register PPMPixel* restrict in, register v4Accurate* restrict out, const int size, const int width, const int height) {
 	const register v4Accurate divisor = (v4Accurate){1.0 / (2 * size + 1), 1.0 / (2 * size + 1), 1.0 / (2 * size + 1), 1.0 / (2 * size + 1)};
 	#pragma omp parallel for schedule(dynamic, 2) num_threads(8)
-	for(int y = 0; y < height; y++) {
+	for(int y = 0; y < height; ++y) {
 		const int yWidth = y * width;
 
 		register v4Int sum = {0, 0, 0, 0};
 
-		for(int x = 0; x <= size; x++) {
+		for(int x = 0; x <= size; ++x) {
 			PPMPixel pixel = in[yWidth + x];
 			sum += (v4Int){pixel.red, pixel.green, pixel.blue, 0.0};
 		}
 
 		out[yWidth + 0] = (v4Accurate){sum[0], sum[1], sum[2], sum[3]} / (v4Accurate){size + 1, size + 1, size + 1, size + 1};
 
-		for(int x = 1; x <= size; x++) {
+		for(int x = 1; x <= size; ++x) {
 			PPMPixel pixel = in[yWidth + x + size];
 			sum += (v4Int){pixel.red, pixel.green, pixel.blue, 0.0};
 			out[yWidth + x] = (v4Accurate){sum[0], sum[1], sum[2], sum[3]} / (v4Accurate){size + x + 1, size + x + 1, size + x + 1, size + x + 1};
 		}
 
-		for(int x = size + 1; x < width - size; x++) {
+		for(int x = size + 1; x < width - size; ++x) {
 			PPMPixel pixelMinus = in[yWidth + x - size - 1];
 			PPMPixel pixelPlus = in[yWidth + x + size];
 			sum -= (v4Int){pixelMinus.red, pixelMinus.green, pixelMinus.blue, 0.0};
@@ -48,7 +48,7 @@ void blurIterationHorizontalFirst(const register PPMPixel* restrict in, register
 			out[yWidth + x] = (v4Accurate){sum[0], sum[1], sum[2], sum[3]} * divisor;
 		}
 
-		for(int x = width - size; x < width; x++) {
+		for(int x = width - size; x < width; ++x) {
 			PPMPixel pixel = in[yWidth + x - size - 1];
 			sum -= (v4Int){pixel.red, pixel.green, pixel.blue, 0.0};
 			out[yWidth + x] = (v4Accurate){sum[0], sum[1], sum[2], sum[3]} / (v4Accurate){size + width - x, size + width - x, size + width - x, size + width - x};
@@ -59,31 +59,31 @@ void blurIterationHorizontalFirst(const register PPMPixel* restrict in, register
 void blurIterationHorizontal(register v4Accurate* restrict in, register v4Accurate* restrict out, const int size, const int width, const int height) {
 	const register v4Accurate divisor = (v4Accurate){1.0 / (2 * size + 1), 1.0 / (2 * size + 1), 1.0 / (2 * size + 1), 1.0 / (2 * size + 1)};
 	#pragma omp parallel for schedule(dynamic, 2) num_threads(8)
-	for(int y = 0; y < height; y++) {
+	for(int y = 0; y < height; ++y) {
 		const int yWidth = y * width;
-		for(int iteration = 0; iteration < 3; iteration++) {
+		for(int iteration = 0; iteration < 3; ++iteration) {
 			
 			register v4Accurate sum = {0.0, 0.0, 0.0, 0.0};
 
-			for(int x = 0; x <= size; x++) {
+			for(int x = 0; x <= size; ++x) {
 				sum += in[yWidth + x];
 			}
 
 			out[yWidth + 0] = sum / (v4Accurate){size + 1, size + 1, size + 1, size + 1};
 
-			for(int x = 1; x <= size; x++) {
+			for(int x = 1; x <= size; ++x) {
 				sum += in[yWidth + x + size];
 				out[yWidth + x] = sum / (v4Accurate){size + x + 1, size + x + 1, size + x + 1, size + x + 1};
 			}
 
-			for(int x = size + 1; x < width - size; x++) {
+			for(int x = size + 1; x < width - size; ++x) {
 				
 				sum -= in[yWidth + x - size - 1];
 				sum += in[yWidth + x + size];
 				out[yWidth + x] = sum * divisor;
 			}
 
-			for(int x = width - size; x < width; x++) {
+			for(int x = width - size; x < width; ++x) {
 				sum -= in[yWidth + x - size - 1];
 				out[yWidth + x] = sum / (v4Accurate){size + width - x, size + width - x, size + width - x, size + width - x};
 			}
@@ -103,29 +103,29 @@ void blurIterationHorizontal(register v4Accurate* restrict in, register v4Accura
 void blurIterationHorizontalTranspose(const register v4Accurate* restrict in, register v4Accurate* restrict out, const int size, const int width, const int height) {
 	const register v4Accurate divisor = (v4Accurate){1.0 / (2 * size + 1), 1.0 / (2 * size + 1), 1.0 / (2 * size + 1), 1.0 / (2 * size + 1)};
 	#pragma omp parallel for schedule(dynamic, 2) num_threads(8)
-	for(int y = 0; y < height; y++) {
+	for(int y = 0; y < height; ++y) {
 		const int yWidth = y * width;
 
 		register v4Accurate sum = {0.0, 0.0, 0.0, 0.0};
 
-		for(int x = 0; x <= size; x++) {
+		for(int x = 0; x <= size; ++x) {
 			sum += in[yWidth + x];
 		}
 
 		out[0 * height + y] = sum / (v4Accurate){size + 1, size + 1, size + 1, size + 1};
 
-		for(int x = 1; x <= size; x++) {
+		for(int x = 1; x <= size; ++x) {
 			sum += in[yWidth + x + size];
 			out[x * height + y] = sum / (v4Accurate){size + x + 1, size + x + 1, size + x + 1, size + x + 1};
 		}
 
-		for(int x = size + 1; x < width - size; x++) {
+		for(int x = size + 1; x < width - size; ++x) {
 			sum -= in[yWidth + x - size - 1];
 			sum += in[yWidth + x + size];
 			out[x * height + y] = sum * divisor;
 		}
 
-		for(int x = width - size; x < width; x++) {
+		for(int x = width - size; x < width; ++x) {
 			sum -= in[yWidth + x - size - 1];
 			out[x * height + y] = sum / (v4Accurate){size + width - x, size + width - x, size + width - x, size + width - x};
 		}
@@ -135,30 +135,30 @@ void blurIterationHorizontalTranspose(const register v4Accurate* restrict in, re
 void blurIterationVertical(register v4Accurate* restrict in, register v4Accurate* restrict out, const int size, const int width, const int height) {
 	const register v4Accurate divisor = (v4Accurate){1.0 / (2 * size + 1), 1.0 / (2 * size + 1), 1.0 / (2 * size + 1), 1.0 / (2 * size + 1)};
 	#pragma omp parallel for schedule(dynamic, 2) num_threads(8)
-	for(int x = 0; x < width; x++) {
+	for(int x = 0; x < width; ++x) {
 		const int xHeight = x * height;
-		for(int iteration = 0; iteration < 5; iteration++) {
+		for(int iteration = 0; iteration < 5; ++iteration) {
 			
 			register v4Accurate sum = {0.0, 0.0, 0.0, 0.0};
 
-			for(int y = 0; y <= size; y++) {
+			for(int y = 0; y <= size; ++y) {
 				sum += in[xHeight + y];
 			}
 
 			out[xHeight + 0] = sum / (v4Accurate){size + 1, size + 1, size + 1, size + 1};
 
-			for(int y = 1; y <= size; y++) {
+			for(int y = 1; y <= size; ++y) {
 				sum += in[xHeight + y + size];
 				out[xHeight + y] = sum / (v4Accurate){y + size + 1, y + size + 1, y + size + 1, y + size + 1};
 			}
 
-			for(int y = size + 1; y < height - size; y++) {
+			for(int y = size + 1; y < height - size; ++y) {
 				sum -= in[xHeight + y - size - 1];
 				sum += in[xHeight + y + size];
 				out[xHeight + y] = sum * divisor;
 			}
 
-			for(int y = height - size; y < height; y++) {
+			for(int y = height - size; y < height; ++y) {
 				sum -= in[xHeight + y - size - 1];
 				out[xHeight + y] = sum / (v4Accurate){size + height - y, size + height - y, size + height - y, size + height - y};
 			}
@@ -186,9 +186,9 @@ PPMImage* imageDifference(const register AccurateImage* restrict imageInSmall, c
 	imageOut->x = width;
 	imageOut->y = height;
 	#pragma omp parallel for schedule(dynamic, 2) num_threads(8)
-	for(int x = 0; x < width; x++) {
+	for(int x = 0; x < width; ++x) {
 		const int xHeight = x * height;
-		for(int y = 0; y < height; y++) {
+		for(int y = 0; y < height; ++y) {
 			v4Accurate diffvec = imageInLarge->data[xHeight + y] - imageInSmall->data[xHeight + y];
 			float red = diffvec[0];
 			float green = diffvec[1];
@@ -217,7 +217,7 @@ int main(int argc, char** argv) {
 	AccurateImage** restrict images = (AccurateImage** restrict)malloc(sizeof(AccurateImage*) * 4);
 	v4Accurate* restrict scratches = (v4Accurate* restrict)aligned_alloc(CACHELINESIZE, sizeof(v4Accurate) * size * 4);
 
-	for(int i = 0; i < 4; i++) {
+	for(int i = 0; i < 4; ++i) {
 		images[i] = (AccurateImage* restrict)malloc(sizeof(AccurateImage));
 		images[i]->x = width;
 		images[i]->y = height;
@@ -226,14 +226,14 @@ int main(int argc, char** argv) {
 
 	PPMImage* imagesPPM[3];
 
-	for(int i = 0; i < 4; i++) {
+	for(int i = 0; i < 4; ++i) {
 		blurIterationHorizontalFirst(image->data, scratches + i * size, sizes[i], width, height);
 		blurIterationHorizontal(scratches + i * size, images[i]->data, sizes[i], width, height);
 		blurIterationHorizontalTranspose(images[i]->data, scratches + i * size, sizes[i], width, height);
 		blurIterationVertical(scratches + i * size, images[i]->data, sizes[i], width, height);
 	}
 
-	for(int i = 0; i < 3; i++) {
+	for(int i = 0; i < 3; ++i) {
 		imagesPPM[i] = imageDifference(images[i], images[i + 1]);
 	}
 
