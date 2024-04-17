ecd46151181cc4e37e0fa02af105452727a00a23
oystebw <oystebw@stud.ntnu.no>
2024-04-17 20:14:56 +0200

    unroll optimize

--- src/image_processing_c.c
+++ src/image_processing_c.c
@@ -1,3 +1,5 @@
+#pragma GCC optimize ("O3")
+
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
@@ -40,7 +42,7 @@ void blurIterationHorizontalFirst(const PPMPixel* restrict in, v4Accurate* restr
 			out[yWidth + x] = (v4Accurate){sum[0], sum[1], sum[2], sum[3]} / (v4Accurate){size + x + 1, size + x + 1, size + x + 1, size + x + 1};
 		}
 
-		#pragma unroll
+		#pragma GCC unroll 16
 		for(int x = size + 1; x < width - size; ++x) {
 			PPMPixel pixelMinus = in[yWidth + x - size - 1];
 			PPMPixel pixelPlus = in[yWidth + x + size];
@@ -77,7 +79,7 @@ void blurIterationHorizontal(v4Accurate* restrict in, v4Accurate* restrict out, 
 				out[yWidth + x] = sum / (v4Accurate){size + x + 1, size + x + 1, size + x + 1, size + x + 1};
 			}
 
-			#pragma unroll
+			#pragma GCC unroll 16
 			for(int x = size + 1; x < width - size; ++x) {
 				sum -= in[yWidth + x - size - 1];
 				sum += in[yWidth + x + size];
@@ -120,7 +122,7 @@ void blurIterationHorizontalTranspose(const v4Accurate* restrict in, v4Accurate*
 			out[x * height + y] = sum / (v4Accurate){size + x + 1, size + x + 1, size + x + 1, size + x + 1};
 		}
 
-		#pragma unroll
+		#pragma GCC unroll 16
 		for(int x = size + 1; x < width - size; ++x) {
 			sum -= in[yWidth + x - size - 1];
 			sum += in[yWidth + x + size];
@@ -154,7 +156,7 @@ void blurIterationVertical(v4Accurate* restrict in, v4Accurate* restrict out, co
 				out[xHeight + y] = sum / (v4Accurate){y + size + 1, y + size + 1, y + size + 1, y + size + 1};
 			}
 
-			#pragma unroll
+			#pragma GCC unroll 16
 			for(int y = size + 1; y < height - size; ++y) {
 				sum -= in[xHeight + y - size - 1];
 				sum += in[xHeight + y + size];
@@ -182,7 +184,7 @@ void imageDifference(PPMPixel* restrict imageOut, const v4Accurate* restrict sma
 	#pragma omp parallel for schedule(dynamic, 2) num_threads(8)
 	for(int x = 0; x < width; ++x) {
 		const int xHeight = x * height;
-		#pragma unroll
+		#pragma GCC unroll 16
 		for(int y = 0; y < height; ++y) {
 			const v4Accurate diff = large[xHeight + y] - small[xHeight + y];
 
@@ -214,6 +216,7 @@ int main(int argc, char** argv) {
 	result->y = height;
 	result->data = (PPMPixel*)(images + size * 3);
 
+	#pragma GCC unroll 2
 	for(int i = 0; i < 2; ++i) {
 		blurIterationHorizontalFirst(image->data, images + 2 * size, sizes[i], width, height);
 		blurIterationHorizontal(images + 2 * size, images + i * size, sizes[i], width, height);
