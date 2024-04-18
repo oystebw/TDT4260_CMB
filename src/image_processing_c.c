dbfa01b7801290fbf909298a960ff3fb240b489c
oystebw <oystebw@stud.ntnu.no>
2024-04-18 17:13:47 +0200

    pf offset 8

--- src/image_processing_c.c
+++ src/image_processing_c.c
@@ -9,7 +9,7 @@
 
 #define CACHELINESIZE 16
 #define BLOCKSIZE 16
-#define PF_OFFSET 4
+#define PF_OFFSET 8
 
 typedef float v4Accurate __attribute__((vector_size(16)));
 typedef __uint32_t v4Int __attribute__((vector_size(16)));
