c219780a539078f990e9f9c7f61395cc9d2b9b14
oystebw <oystebw@stud.ntnu.no>
2024-04-18 16:47:57 +0200

    read prefetch

--- src/image_processing_c.c
+++ src/image_processing_c.c
@@ -124,10 +124,10 @@ void blurIterationHorizontalTranspose(const v4Accurate* restrict in, v4Accurate*
 
 		#pragma GCC unroll 16
 		for(int x = size + 1; x < width - size; ++x) {
-			__builtin_prefetch(&out[(x + 4) * height + y], 1, 0);
 			sum -= in[yWidth + x - size - 1];
 			sum += in[yWidth + x + size];
 			out[x * height + y] = sum * divisor;
+			__builtin_prefetch(&in[yWidth + x + 16], 0, 3);
 		}
 
 		for(int x = width - size; x < width; ++x) {
