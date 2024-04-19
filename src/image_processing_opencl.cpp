d9de74b7206e0394ea730ea590f186fa4a344e7b
oystebw <oystebw@stud.ntnu.no>
2024-04-19 21:52:03 +0200

    pics

--- src/image_processing_opencl.cpp
+++ src/image_processing_opencl.cpp
@@ -40,155 +40,135 @@ void errorAndExit(string error_message) {
     exit(1);
 }
 
-// class OpenClBlur{
-// public:
-//     OpenClBlur() {
+class OpenClBlur{
+public:
+    OpenClBlur() {
         
-//         // choose a platform containing this string if available
-//         string preferred_platform = "Intel";
-//         // select platform
-//         std::vector<Platform> all_platforms;
-//         Platform::get(&all_platforms);
-//         if (all_platforms.empty()){
-//             errorAndExit("No platforms found.\n");
-//         }
-//         Platform default_platform = all_platforms[0];
-//         for(const auto& platform: all_platforms){
-//             cerr << "Found platform: " << platform.getInfo<CL_PLATFORM_NAME>() << endl;
-//             if(platform.getInfo<CL_PLATFORM_NAME>().find(preferred_platform)!=-1){
-//                 default_platform = platform;
-//             }
-//         }
-//         cerr << "Using platform: " << default_platform.getInfo<CL_PLATFORM_NAME>() << "\n";
+        // choose a platform containing this string if available
+        string preferred_platform = "Intel";
+        // select platform
+        std::vector<Platform> all_platforms;
+        Platform::get(&all_platforms);
+        if (all_platforms.empty()){
+            errorAndExit("No platforms found.\n");
+        }
+        Platform default_platform = all_platforms[0];
+        for(const auto& platform: all_platforms){
+            cerr << "Found platform: " << platform.getInfo<CL_PLATFORM_NAME>() << endl;
+            if(platform.getInfo<CL_PLATFORM_NAME>().find(preferred_platform)!=-1){
+                default_platform = platform;
+            }
+        }
+        cerr << "Using platform: " << default_platform.getInfo<CL_PLATFORM_NAME>() << "\n";
 
-//         // select device
-//         std::vector<Device> all_devices;
-//         default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
-//         if (all_devices.empty()){
-//             errorAndExit("No devices found.");
-//         }
-//         Device default_device = all_devices[0];
-//         for(const auto& device: all_devices){
-//             cerr << "Found device: " << device.getInfo<CL_DEVICE_NAME>() << endl;
-//         }
-//         cerr << "Using device: " << default_device.getInfo<CL_DEVICE_NAME>() << "\n";
-//         device = default_device;
+        // select device
+        std::vector<Device> all_devices;
+        default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
+        if (all_devices.empty()){
+            errorAndExit("No devices found.");
+        }
+        Device default_device = all_devices[0];
+        for(const auto& device: all_devices){
+            cerr << "Found device: " << device.getInfo<CL_DEVICE_NAME>() << endl;
+        }
+        cerr << "Using device: " << default_device.getInfo<CL_DEVICE_NAME>() << "\n";
+        device = default_device;
 
 
-//         for(const auto& foundDevice : all_devices) {
-//             if(foundDevice.getInfo<CL_DEVICE_TYPE>() == CL_DEVICE_TYPE_GPU) {
-//                 device = foundDevice;
-//                 break;
-//             }
-//         }
+        for(const auto& foundDevice : all_devices) {
+            if(foundDevice.getInfo<CL_DEVICE_TYPE>() == CL_DEVICE_TYPE_GPU) {
+                device = foundDevice;
+                break;
+            }
+        }
 
-//         // create context
-//         context = Context({device});
+        // create context
+        context = Context({device});
 
-//         // create command queue with profiling enabled
-//         queue = CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE);
+        // create command queue with profiling enabled
+        queue = CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE);
 
-//         // load opencl program and compile it
-//         Program::Sources sources;
-//         ifstream ifs("image_processing_opencl.cl");
-//         if (ifs.fail()){
-//             errorAndExit("Failed to open image_processing_opencl.");
-//         }
-//         string kernel_source { istreambuf_iterator<char>(ifs), istreambuf_iterator<char>() };
-//         sources.push_back({kernel_source.c_str(), kernel_source.size()});
-//         program = Program(context, sources);
-//         if (program.build({device}) != CL_SUCCESS){
-//             errorAndExit("Error building program!");
-//         }
+        // load opencl program and compile it
+        Program::Sources sources;
+        ifstream ifs("image_processing_opencl.cl");
+        if (ifs.fail()){
+            errorAndExit("Failed to open image_processing_opencl.");
+        }
+        string kernel_source { istreambuf_iterator<char>(ifs), istreambuf_iterator<char>() };
+        sources.push_back({kernel_source.c_str(), kernel_source.size()});
+        program = Program(context, sources);
+        if (program.build({device}) != CL_SUCCESS){
+            errorAndExit("Error building program!");
+        }
 
-//         kernelHorizontal = Kernel(program, "kernelHorizontal");
-//         kernelVertical = Kernel(program, "kernelVertical");
-//     }
-//     AccurateImage* blur(AccurateImage* image, int size){
+        kernelDiff = Kernel(program, "kernelDiff");
+    }
 
-//         std::size_t bufferSize = image->x * image->y * sizeof(AccuratePixel);
-//         Buffer buffer1(context, CL_MEM_READ_WRITE|CL_MEM_READ_WRITE, bufferSize);
-//         Buffer buffer2(context, CL_MEM_READ_WRITE|CL_MEM_READ_WRITE, bufferSize);
+    void imageDiff(PPMPixel* result, const v4Accurate* large, const v4Accurate* small, const int width, const int height){
+        const int size = width * height;
+        Buffer largebuff(context, CL_MEM_READ_WRITE|CL_MEM_READ_WRITE, size * sizeof(v4Accurate));
+        Buffer smallbuff(context, CL_MEM_READ_WRITE|CL_MEM_READ_WRITE, size * sizeof(v4Accurate));
+        Buffer resultbuff(context, CL_MEM_READ_WRITE|CL_MEM_READ_WRITE, size * sizeof(PPMPixel));
 
-//         events.emplace_back(make_pair("copy buffer to image", Event()));
+        events.emplace_back(make_pair("copy buffer to image", Event()));
 
-//         queue.enqueueWriteBuffer(buffer1, false, 0, bufferSize, image->data, nullptr, &events.back().second);
+        queue.enqueueWriteBuffer(largebuff, false, 0, size * sizeof(v4Accurate), large, nullptr, &events.back().second);
+        queue.enqueueWriteBuffer(smallbuff, false, 0, size * sizeof(v4Accurate), small, nullptr, &events.back().second);
 
-//         blurIteration(image, buffer1, buffer2, size);
+        diff(resultbuff, largebuff, smallbuff, width, height);
 
-//         AccurateImage* result = copyAccurateImage(image, true, false);
+        events.emplace_back(make_pair("map buffer in memory", Event()));
 
-//         events.emplace_back(make_pair("map buffer in memory", Event()));
+        result = (PPMPixel*)queue.enqueueMapBuffer(resultbuff, CL_FALSE, CL_MAP_READ, 0, size * sizeof(PPMPixel), nullptr, &events.back().second);
+    }
+    void finish(){
+        // finish execution and print events
+        queue.finish();
+        for(auto [s, e]: events){
+            printEvent(s, e);
+        }
+        events.clear();
+    }
 
-//         result->data = (AccuratePixel*)queue.enqueueMapBuffer(buffer1, CL_FALSE, CL_MAP_READ, 0, bufferSize, nullptr, &events.back().second);
-//         return result;
-//     }
-//     void finish(){
-//         // finish execution and print events
-//         queue.finish();
-//         for(auto [s, e]: events){
-//             printEvent(s, e);
-//         }
-//         events.clear();
-//     }
+private:
+    Device device;
+    Context context;
+    CommandQueue queue;
+    Program program;
+    Kernel kernelDiff;
+    std::vector<pair<string, Event>> events;
+    void printEvent(string s, Event& evt){
 
-// private:
-//     Device device;
-//     Context context;
-//     CommandQueue queue;
-//     Program program;
-//     Kernel kernelHorizontal;
-//     Kernel kernelVertical;
-//     std::vector<pair<string, Event>> events;
-//     void printEvent(string s, Event& evt){
-
-//         // ensure the event has completed
-//         assert(evt.getInfo<CL_EVENT_COMMAND_EXECUTION_STATUS>()==CL_COMPLETE);
-//         cl_ulong queued = evt.getProfilingInfo<CL_PROFILING_COMMAND_QUEUED>();
-//         cl_ulong submit = evt.getProfilingInfo<CL_PROFILING_COMMAND_SUBMIT>();
-//         cl_ulong start = evt.getProfilingInfo<CL_PROFILING_COMMAND_START>();
-//         cl_ulong end = evt.getProfilingInfo<CL_PROFILING_COMMAND_END>();
-//         cerr << "event: " << s << endl;
-//         cerr << "queue time: " << submit-queued << "ns" << endl;
-//         cerr << "run time: " << end-start << "ns" << endl;
-//         cerr << "total time: " << end-queued << "ns" << endl;
-//     }
-//     void blurIteration(AccurateImage* image, Buffer& src, Buffer& dst, cl_int size){
-
-//         // create Event for profiling
-//         events.emplace_back(make_pair("kernelHorizontal", Event()));
-//         // set call arguments
-//         kernelHorizontal.setArg(0, src);
-//         kernelHorizontal.setArg(1, dst);
-//         kernelHorizontal.setArg(2, image->x);
-//         kernelHorizontal.setArg(3, size);
-//         // call 2D kernel
-//         queue.enqueueNDRangeKernel(
-//                 kernelHorizontal, // kernel to queue
-//                 NullRange, // use no offset
-//                 NDRange(image->y), // 1D kernel
-//                 NullRange, // use no local range
-//                 nullptr, // we use the queue in sequential mode so we don't have to specify Events that need to finish before
-//                 &events.back().second // Event to use for profiling
-//         );
-
-//         events.emplace_back(make_pair("kernelVertical", Event()));
-//         // set call arguments
-//         kernelVertical.setArg(0, dst);
-//         kernelVertical.setArg(1, src);
-//         kernelVertical.setArg(2, image->y);
-//         kernelVertical.setArg(3, size);
-//         // call 2D kernel
-//         queue.enqueueNDRangeKernel(
-//                 kernelVertical, // kernel to queue
-//                 NullRange, // use no offset
-//                 NDRange(image->x), // 1D kernel
-//                 NullRange, // use no local range
-//                 nullptr, // we use the queue in sequential mode so we don't have to specify Events that need to finish before
-//                 &events.back().second // Event to use for profiling
-//         );
-//     }
-// };
+        // ensure the event has completed
+        assert(evt.getInfo<CL_EVENT_COMMAND_EXECUTION_STATUS>()==CL_COMPLETE);
+        cl_ulong queued = evt.getProfilingInfo<CL_PROFILING_COMMAND_QUEUED>();
+        cl_ulong submit = evt.getProfilingInfo<CL_PROFILING_COMMAND_SUBMIT>();
+        cl_ulong start = evt.getProfilingInfo<CL_PROFILING_COMMAND_START>();
+        cl_ulong end = evt.getProfilingInfo<CL_PROFILING_COMMAND_END>();
+        cerr << "event: " << s << endl;
+        cerr << "queue time: " << submit-queued << "ns" << endl;
+        cerr << "run time: " << end-start << "ns" << endl;
+        cerr << "total time: " << end-queued << "ns" << endl;
+    }
+    
+    void diff(Buffer& resultbuff, Buffer&largebuff, Buffer& smallbuff, const int width, const int height){
+        events.emplace_back(make_pair("kernelDiff", Event()));
+        // set call arguments
+        kernelDiff.setArg(0, resultbuff);
+        kernelDiff.setArg(1, largebuff);
+        kernelDiff.setArg(2, smallbuff);
+        kernelDiff.setArg(3, height);
+        // call 2D kernel
+        queue.enqueueNDRangeKernel(
+                kernelDiff, // kernel to queue
+                NullRange, // use no offset
+                NDRange(width), // 1D kernel
+                NullRange, // use no local range
+                nullptr, // we use the queue in sequential mode so we don't have to specify Events that need to finish before
+                &events.back().second // Event to use for profiling
+        );
+    }
+};
 
 
 
@@ -419,6 +399,11 @@ int main(int argc, char** argv) {
 	blurIterationHorizontal( scratch,  two,  8, width, height);
 	blurIterationHorizontalTranspose( two,  scratch,  8, width, height);
 	blurIterationVertical( scratch,  two,  8, width, height);
-	imageDifference(result_data,  one,  two, width, height);
+	// imageDifference(result_data,  one,  two, width, height);
+
+    OpenClBlur blur;
+    blur.imageDiff(result_data, two, one, width, height);
+    blur.finish();
+
 	(argc > 1) ? writePPM("flower_medium.ppm", result) : writeStreamPPM(stdout, result);
 }
