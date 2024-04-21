4576e34c9dbf88696edff4d97195eeb9eca8b82b
oystebw <oystebw@stud.ntnu.no>
2024-04-21 22:52:56 +0200

    pics

--- src/image_processing_c.c
+++ src/image_processing_c.c
@@ -16,9 +16,7 @@ typedef float v4Accurate __attribute__((vector_size(16)));
 typedef __uint32_t v4Int __attribute__((vector_size(16)));
 
 PPMPixel result_data[1920 * 1200];
-v4Accurate one[1920 * 1200];
-v4Accurate two[1920 * 1200];
-v4Accurate scratch[1920 * 1200];
+v4Accurate accurate[1920 * 1200 * 3];
 
 // Image from:
 // http://7-themes.com/6971875-funny-flowers-pictures.html
@@ -39,19 +37,19 @@ void blurIterationHorizontalFirst(const PPMPixel* restrict in, v4Accurate* restr
 
 		for(int x = 1; x <= size; ++x) {
 			sum += (v4Int){in[yWidth + x + size].red, in[yWidth + x + size].green, in[yWidth + x + size].blue, 0.0};
-			out[yWidth + x] = (v4Accurate){sum[0], sum[1], sum[2], sum[3]} / (v4Accurate){size + x + 1, size + x + 1, size + x + 1, 1.0f};
+			out[yWidth + x * 3] = (v4Accurate){sum[0], sum[1], sum[2], sum[3]} / (v4Accurate){size + x + 1, size + x + 1, size + x + 1, 1.0f};
 		}
 
 		#pragma GCC unroll 16
 		for(int x = size + 1; x < width - size; ++x) {
 			sum -= (v4Int){in[yWidth + x - size - 1].red, in[yWidth + x - size - 1].green, in[yWidth + x - size - 1].blue, 0.0};
 			sum += (v4Int){in[yWidth + x + size].red, in[yWidth + x + size].green, in[yWidth + x + size].blue, 0.0};
-			out[yWidth + x] = (v4Accurate){sum[0], sum[1], sum[2], sum[3]} * divisor;
+			out[yWidth + x * 3] = (v4Accurate){sum[0], sum[1], sum[2], sum[3]} * divisor;
 		}
 
 		for(int x = width - size; x < width; ++x) {
 			sum -= (v4Int){in[yWidth + x - size - 1].red, in[yWidth + x - size - 1].green, in[yWidth + x - size - 1].blue, 0.0};
-			out[yWidth + x] = (v4Accurate){sum[0], sum[1], sum[2], sum[3]} / (v4Accurate){size + width - x, size + width - x, size + width - x, 1.0f};
+			out[yWidth + x * 3] = (v4Accurate){sum[0], sum[1], sum[2], sum[3]} / (v4Accurate){size + width - x, size + width - x, size + width - x, 1.0f};
 		}
 	}
 }
@@ -66,26 +64,26 @@ void blurIterationHorizontal(v4Accurate* in, v4Accurate* out, const int size, co
 			v4Accurate sum = {0.0, 0.0, 0.0, 0.0};
 
 			for(int x = 0; x <= size; ++x) {
-				sum += in[yWidth + x];
+				sum += in[yWidth + x * 3];
 			}
 
 			out[yWidth + 0] = sum / (v4Accurate){size + 1, size + 1, size + 1, 1.0f};
 
 			for(int x = 1; x <= size; ++x) {
-				sum += in[yWidth + x + size];
-				out[yWidth + x] = sum / (v4Accurate){size + x + 1, size + x + 1, size + x + 1, 1.0f};
+				sum += in[yWidth + x * 3 + size];
+				out[yWidth + x * 3] = sum / (v4Accurate){size + x + 1, size + x + 1, size + x + 1, 1.0f};
 			}
 
 			#pragma GCC unroll 16
 			for(int x = size + 1; x < width - size; ++x) {
-				sum -= in[yWidth + x - size - 1];
-				sum += in[yWidth + x + size];
-				out[yWidth + x] = sum * divisor;
+				sum -= in[yWidth + x * 3 - size - 1];
+				sum += in[yWidth + x * 3 + size];
+				out[yWidth + x * 3] = sum * divisor;
 			}
 
 			for(int x = width - size; x < width; ++x) {
-				sum -= in[yWidth + x - size - 1];
-				out[yWidth + x] = sum / (v4Accurate){size + width - x, size + width - x, size + width - x, 1.0f};
+				sum -= in[yWidth + x * 3 - size - 1];
+				out[yWidth + x * 3] = sum / (v4Accurate){size + width - x, size + width - x, size + width - x, 1.0f};
 			}
 
 			// swap in and out
@@ -109,26 +107,26 @@ void blurIterationHorizontalTranspose(const v4Accurate* restrict in, v4Accurate*
 		v4Accurate sum = {0.0, 0.0, 0.0, 0.0};
 
 		for(int x = 0; x <= size; ++x) {
-			sum += in[yWidth + x];
+			sum += in[yWidth + x * 3];
 		}
 
 		out[0 * height + y] = sum / (v4Accurate){size + 1, size + 1, size + 1, 1.0f};
 
 		for(int x = 1; x <= size; ++x) {
-			sum += in[yWidth + x + size];
-			out[x * height + y] = sum / (v4Accurate){size + x + 1, size + x + 1, size + x + 1, 1.0f};
+			sum += in[yWidth + x * 3 + size];
+			out[x * 3 * height + y] = sum / (v4Accurate){size + x + 1, size + x + 1, size + x + 1, 1.0f};
 		}
 
 		#pragma GCC unroll 16
 		for(int x = size + 1; x < width - size; ++x) {			
-			sum -= in[yWidth + x - size - 1];
-			sum += in[yWidth + x + size];
-			out[x * height + y] = sum * divisor;
+			sum -= in[yWidth + x * 3 - size - 1];
+			sum += in[yWidth + x * 3 + size];
+			out[x * 3 * height + y] = sum * divisor;
 		}
 
 		for(int x = width - size; x < width; ++x) {
-			sum -= in[yWidth + x - size - 1];
-			out[x * height + y] = sum / (v4Accurate){size + width - x, size + width - x, size + width - x, 1.0f};
+			sum -= in[yWidth + x * 3 - size - 1];
+			out[x * 3 * height + y] = sum / (v4Accurate){size + width - x, size + width - x, size + width - x, 1.0f};
 		}
 	}
 }
@@ -142,26 +140,26 @@ void blurIterationVertical(v4Accurate* in, v4Accurate* out, const int size, cons
 			v4Accurate sum = {0.0, 0.0, 0.0, 0.0};
 
 			for(int y = 0; y <= size; ++y) {
-				sum += in[xHeight + y];
+				sum += in[xHeight + y * 3];
 			}
 
 			out[xHeight + 0] = sum / (v4Accurate){size + 1, size + 1, size + 1, 1.0f};
 
 			for(int y = 1; y <= size; ++y) {
-				sum += in[xHeight + y + size];
-				out[xHeight + y] = sum / (v4Accurate){y + size + 1, y + size + 1, y + size + 1, 1.0f};
+				sum += in[xHeight + y * 3 + size];
+				out[xHeight + y * 3] = sum / (v4Accurate){y + size + 1, y + size + 1, y + size + 1, 1.0f};
 			}
 
 			#pragma GCC unroll 16
 			for(int y = size + 1; y < height - size; ++y) {
-				sum -= in[xHeight + y - size - 1];
-				sum += in[xHeight + y + size];
-				out[xHeight + y] = sum * divisor;
+				sum -= in[xHeight + y * 3 - size - 1];
+				sum += in[xHeight + y * 3 + size];
+				out[xHeight + y * 3] = sum * divisor;
 			}
 
 			for(int y = height - size; y < height; ++y) {
-				sum -= in[xHeight + y - size - 1];
-				out[xHeight + y] = sum / (v4Accurate){size + height - y, size + height - y, size + height - y, 1.0f};
+				sum -= in[xHeight + y * 3 - size - 1];
+				out[xHeight + y * 3] = sum / (v4Accurate){size + height - y, size + height - y, size + height - y, 1.0f};
 			}
 			// swap
 			v4Accurate* tmp = in;
@@ -186,7 +184,7 @@ void imageDifference(PPMPixel* restrict imageOut, const v4Accurate* restrict sma
 				__builtin_prefetch((float*)&small[xHeight + height + yy], 0, 3);
 				#pragma GGC unroll 8
 				for(int y = yy; y < yy + BLOCKSIZE; ++y) {
-					v4Accurate diff = large[xHeight + y] - small[xHeight + y];
+					v4Accurate diff = large[xHeight + y * 3] - small[xHeight + y * 3];
 					imageOut[y * width + x] = (PPMPixel){
 						diff[0] = diff[0] < 0.0 ? diff[0] + 257.0 : diff[0],
 						diff[1] = diff[1] < 0.0 ? diff[1] + 257.0 : diff[1],
@@ -200,6 +198,7 @@ void imageDifference(PPMPixel* restrict imageOut, const v4Accurate* restrict sma
 
 int main(int argc, char** argv) {
 
+
     const PPMImage* restrict image = (argc > 1) ? readPPM("flower.ppm") : readStreamPPM(stdin);
 
 	const int width = image->x;
@@ -211,31 +210,31 @@ int main(int argc, char** argv) {
 	result->y = height;
 	result->data = result_data;
 
-	blurIterationHorizontalFirst(image->data,  scratch,  2, width, height);
-	blurIterationHorizontal( scratch,  one,  2, width, height);
-	blurIterationHorizontalTranspose( one,  scratch,  2, width, height);
-	blurIterationVertical( scratch,  one,  2, width, height);
+	blurIterationHorizontalFirst(image->data,  accurate,  2, width, height);
+	blurIterationHorizontal( accurate,  accurate + 1,  2, width, height);
+	blurIterationHorizontalTranspose( accurate + 1,  accurate,  2, width, height);
+	blurIterationVertical( accurate,  accurate + 1,  2, width, height);
 
-	blurIterationHorizontalFirst(image->data,  scratch,  3, width, height);
-	blurIterationHorizontal( scratch,  two,  3, width, height);
-	blurIterationHorizontalTranspose( two,  scratch,  3, width, height);
-	blurIterationVertical( scratch,  two,  3, width, height);
+	blurIterationHorizontalFirst(image->data,  accurate,  3, width, height);
+	blurIterationHorizontal( accurate,  accurate + 2,  3, width, height);
+	blurIterationHorizontalTranspose( accurate + 2,  accurate,  3, width, height);
+	blurIterationVertical( accurate,  accurate + 2,  3, width, height);
 
-	imageDifference(result_data,  one,  two, width, height);
+	imageDifference(result_data,  accurate + 1,  accurate + 2, width, height);
 	(argc > 1) ? writePPM("flower_tiny.ppm", result) : writeStreamPPM(stdout, result);
 
-	blurIterationHorizontalFirst(image->data,  scratch,  5, width, height);
-	blurIterationHorizontal( scratch,  one,  5, width, height);
-	blurIterationHorizontalTranspose( one,  scratch,  5, width, height);
-	blurIterationVertical( scratch,  one,  5, width, height);
-	imageDifference(result_data,  two,  one, width, height);
+	blurIterationHorizontalFirst(image->data,  accurate,  5, width, height);
+	blurIterationHorizontal( accurate,  accurate + 1,  5, width, height);
+	blurIterationHorizontalTranspose( accurate + 1,  accurate,  5, width, height);
+	blurIterationVertical( accurate,  accurate + 1,  5, width, height);
+	imageDifference(result_data,  accurate + 2,  accurate + 1, width, height);
 	(argc > 1) ? writePPM("flower_small.ppm", result) : writeStreamPPM(stdout, result);
 
-	blurIterationHorizontalFirst(image->data,  scratch,  8, width, height);
-	blurIterationHorizontal( scratch,  two,  8, width, height);
-	blurIterationHorizontalTranspose( two,  scratch,  8, width, height);
-	blurIterationVertical( scratch,  two,  8, width, height);
-	imageDifference(result_data,  one,  two, width, height);
+	blurIterationHorizontalFirst(image->data,  accurate,  8, width, height);
+	blurIterationHorizontal( accurate,  accurate + 2,  8, width, height);
+	blurIterationHorizontalTranspose( accurate + 2,  accurate,  8, width, height);
+	blurIterationVertical( accurate,  accurate + 2,  8, width, height);
+	imageDifference(result_data,  accurate + 1,  accurate + 2, width, height);
 	(argc > 1) ? writePPM("flower_medium.ppm", result) : writeStreamPPM(stdout, result);
 }
 
