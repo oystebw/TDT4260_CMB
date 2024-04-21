81ffde0e208ecbb9d08014627bf4d2cbe7c3b2aa
oystebw <oystebw@stud.ntnu.no>
2024-04-21 09:26:46 +0200

    pf32

--- src/image_processing_c.c
+++ src/image_processing_c.c
@@ -9,7 +9,7 @@ __attribute__((optimize("prefetch-loop-arrays")))
 #include "ppm.h"
 
 #define BLOCKSIZE 8
-#define PF_OFFSET 16
+#define PF_OFFSET 32
 
 typedef float v4Accurate __attribute__((vector_size(16)));
 typedef __uint32_t v4Int __attribute__((vector_size(16)));
