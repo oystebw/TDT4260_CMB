acb4227cfe46dbf37258db39074ac97c6673749e
oystebw <oystebw@stud.ntnu.no>
2024-04-26 23:09:59 +0200

    only pf in

--- src/image_processing_c.c
+++ src/image_processing_c.c
@@ -190,7 +190,7 @@ __attribute__((hot)) void blurIterationHorizontalAlternative(v4Accurate* restric
 		#pragma GCC unroll 16
 		#pragma GCC ivdep
 		for(int x = 3 * size + 3; x < width - size; ++x) {
-			// __builtin_prefetch(&in[yWidth + x + size + PF_OFFSET], 0, 3); // two cachelines ahead
+			__builtin_prefetch(&in[yWidth + x + size + PF_OFFSET], 0, 3); // two cachelines ahead
 			// __builtin_prefetch(&out[yWidth + x - 1 + PF_OFFSET], 0, 3); // two cachelines ahead
 			out[yWidth + x] = sum1 += in[yWidth + x + size] - in[yWidth + x - size - 1];
 			in[yWidth + x - size - 1] = sum2 += out[yWidth + x - 1] - out[yWidth + x - 2 * size - 2];
@@ -401,7 +401,7 @@ __attribute__((hot)) void blurIterationVerticalAlternative(v4Accurate* restrict 
 		#pragma GCC unroll 16
 		#pragma GCC ivdep
 		for(int y = 5 * size + 5; y < height - size; ++y) {
-			// __builtin_prefetch(&in[xHeight + y + size + PF_OFFSET], 0, 3); // two cachelines ahead
+			__builtin_prefetch(&in[xHeight + y + size + PF_OFFSET], 0, 3); // two cachelines ahead
 			// __builtin_prefetch(&out[xHeight + y - 1 + PF_OFFSET], 0, 3);
 			out[xHeight + y] = sum1 += in[xHeight + y + size] - in[xHeight + y - size - 1];
 			in[xHeight + y - size - 1] = sum2 += out[xHeight + y - 1] - out[xHeight + y - 2 * size - 2];
