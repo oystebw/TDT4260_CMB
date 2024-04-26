61c64827d3add7dd868daa1977b06d8331909c90
oystebw <oystebw@stud.ntnu.no>
2024-04-26 23:34:56 +0200

    even more mulripassing

--- src/image_processing_c.c
+++ src/image_processing_c.c
@@ -135,7 +135,7 @@ __attribute__((hot)) void blurIterationHorizontal(v4Accurate* restrict in, v4Acc
 	}
 }
 
-__attribute__((hot)) void blurIterationHorizontalAlternative(v4Accurate* restrict in, v4Accurate* restrict out, const int size, const int width, const int height) {
+__attribute__((hot)) void blurIterationHorizontalAlternative(const PPMPixel* restrict ppm, v4Accurate* restrict in, v4Accurate* restrict out, const int size, const int width, const int height) {
 	
 	const float sizef = (float)size;
 	const v4Accurate multiplier = (v4Accurate){(2.0f * sizef + 1.0f), (2.0f * sizef + 1.0f), (2.0f * sizef + 1.0f), 1.0f};
@@ -147,18 +147,21 @@ __attribute__((hot)) void blurIterationHorizontalAlternative(v4Accurate* restric
 		register v4Accurate sum1 = {0.0f, 0.0f, 0.0f, 0.0f};
 		register v4Accurate sum2 = {0.0f, 0.0f, 0.0f, 0.0f};
 		register v4Accurate sum3 = {0.0f, 0.0f, 0.0f, 0.0f};
+		register v4Accurate sum4 = {0.0f, 0.0f, 0.0f, 0.0f};
 
 		// setup first iteration
-		for(int x1 = 0; x1 < size + 1; ++x1) {
-			sum1 += in[yWidth + x1];
+		for(int x1 = 0; x1 <= size; ++x1) {
+			sum1 += (v4Accurate){ppm[yWidth + x1].red, ppm[yWidth + x1].green, ppm[yWidth + x1].blue, 0.0f};
 		}
-		out[yWidth + 0] = sum1 * multiplier / (v4Accurate){sizef + 1, sizef + 1, sizef + 1, 1.0f};
+		out[yWidth + 0] = sum1 * multiplier / (v4Accurate){sizef + 1.0f, sizef + 1.0f, sizef + 1.0f, 1.0f};
 		for(int x1 = 1; x1 < size + 1; ++x1) {
-			sum1 += in[yWidth + x1 + size];
-			out[yWidth + x1] = sum1 * multiplier / (v4Accurate){sizef + x1 + 1, sizef + x1 + 1, sizef + x1 + 1, 1.0f};
+			sum1 += (v4Accurate){ppm[yWidth + x1 + size].red, ppm[yWidth + x1 + size].green, ppm[yWidth + x1 + size].blue, 0.0f};
+			out[yWidth + x1] = sum1 * multiplier / (v4Accurate){sizef + x1 + 1.0f, sizef + x1 + 1.0f, sizef + x1 + 1.0f, 1.0f};
 		}
-		for(int x1 = size + 1; x1 < 3 * size + 3; ++x1) {
-			out[yWidth + x1] = sum1 += in[yWidth + x1 + size] - in[yWidth + x1 - size - 1];
+		for(int x1 = size + 1; x1 < 4 * size + 4; ++x1){
+			sum1 -= (v4Accurate){ppm[yWidth + x1 - size - 1].red, ppm[yWidth + x1 - size - 1].green, ppm[yWidth + x1 - size - 1].blue, 0.0f};
+			sum1 += (v4Accurate){ppm[yWidth + x1 + size].red, ppm[yWidth + x1 + size].green, ppm[yWidth + x1 + size].blue, 0.0f};
+			out[yWidth + x1] = sum1;
 		}
 		// setup first iteration
 
@@ -171,7 +174,7 @@ __attribute__((hot)) void blurIterationHorizontalAlternative(v4Accurate* restric
 			sum2 += out[yWidth + x2 + size];
 			in[yWidth + x2] = sum2 * multiplier / (v4Accurate){sizef + x2 + 1, sizef + x2 + 1, sizef + x2 + 1, 1.0f};
 		}
-		for(int x2 = size + 1; x2 < 2 * size + 2; ++x2){
+		for(int x2 = size + 1; x2 < 3 * size + 3; ++x2){
 			in[yWidth + x2] = sum2 += out[yWidth + x2 + size] - out[yWidth + x2 - size - 1];
 		}
 		// setup second iteration
@@ -185,21 +188,38 @@ __attribute__((hot)) void blurIterationHorizontalAlternative(v4Accurate* restric
 			sum3 += in[yWidth + x3 + size];
 			out[yWidth + x3] = sum3 * multiplier / (v4Accurate){sizef + x3 + 1, sizef + x3 + 1, sizef + x3 + 1, 1.0f};
 		}
+		for(int x3 = size + 1; x3 < 2 * size + 2; ++x3){
+			out[yWidth + x3] = sum3 += in[yWidth + x3 + size] - in[yWidth + x3 - size - 1];
+		}
 		// setup third iteration
+
+		// setup fourth iteration
+		for(int x4 = 0; x4 < size + 1; ++x4) {
+			sum4 += out[yWidth + x4];
+		}
+		in[yWidth + 0] = sum4 * multiplier / (v4Accurate){sizef + 1, sizef + 1, sizef + 1, 1.0f};
+		for(int x4 = 1; x4 < size + 1; ++x4) {
+			sum4 += out[yWidth + x4 + size];
+			in[yWidth + x4] = sum4 * multiplier / (v4Accurate){sizef + x4 + 1, sizef + x4 + 1, sizef + x4 + 1, 1.0f};
+		}
+		// setup fourth iteration
 
 		#pragma GCC unroll 16
 		#pragma GCC ivdep
-		for(int x = 3 * size + 3; x < width - size; ++x) {
-			__builtin_prefetch(&in[yWidth + x + size + PF_OFFSET], 0, 3); // two cachelines ahead
+		for(int x = 4 * size + 4; x < width - size; ++x) {
+			__builtin_prefetch(&ppm[yWidth + x + size + 42], 0, 3); // two cachelines ahead
 			// __builtin_prefetch(&out[yWidth + x - 1 + PF_OFFSET], 0, 3); // two cachelines ahead
-			out[yWidth + x] = sum1 += in[yWidth + x + size] - in[yWidth + x - size - 1];
+			sum1 -= (v4Accurate){ppm[yWidth + x - size - 1].red, ppm[yWidth + x - size - 1].green, ppm[yWidth + x - size - 1].blue, 0.0f};
+			sum1 += (v4Accurate){ppm[yWidth + x + size].red, ppm[yWidth + x + size].green, ppm[yWidth + x + size].blue, 0.0f};
+			out[yWidth + x] = sum1;
 			in[yWidth + x - size - 1] = sum2 += out[yWidth + x - 1] - out[yWidth + x - 2 * size - 2];
 			out[yWidth + x - 2 * size - 2] = sum3 += in[yWidth + x - size - 2] - in[yWidth + x - 3 * size - 3];
+			in[yWidth + x - 3 * size - 3] = sum4 += out[yWidth + x - 2 * size - 3] - out[yWidth + x - 4 * size - 4];
 		}
 
 		// finishing first iteration
 		for(int x1 = width - size; x1 < width; ++x1) {
-			sum1 -= in[yWidth + x1 - size - 1];
+			sum1 -= (v4Accurate){ppm[yWidth + x1 - size - 1].red, ppm[yWidth + x1 - size - 1].green, ppm[yWidth + x1 - size - 1].blue, 0.0f};
 			out[yWidth + x1] = sum1 * multiplier / (v4Accurate){sizef + width - x1, sizef + width - x1, sizef + width - x1, 1.0f};
 		}
 		// finishing first iteration
@@ -223,6 +243,16 @@ __attribute__((hot)) void blurIterationHorizontalAlternative(v4Accurate* restric
 			out[yWidth + x3] = sum3 * multiplier / (v4Accurate){sizef + width - x3, sizef + width - x3, sizef + width - x3, 1.0f};
 		}
 		// finishing third iteration
+
+		// finishing fourth iteration
+		for(int x4 = width - 4 * size - 3; x4 < width - size; ++x4) {
+			in[yWidth + x4] = sum4 += out[yWidth + x4 + size] - out[yWidth + x4 - size - 1];
+		}
+		for(int x4 = width - size; x4 < width; ++x4) {
+			sum4 -= out[yWidth + x4 - size - 1];
+			in[yWidth + x4] = sum4 * multiplier / (v4Accurate){sizef + width - x4, sizef + width - x4, sizef + width - x4, 1.0f};
+		}
+		// finishing fourth iteration
 	}
 }
 
@@ -500,8 +530,8 @@ __attribute__((hot)) void imageDifference(PPMPixel* restrict imageOut, const v4A
 Just to clean up and explicitly show what happens to the image.
 */
 void blurImage(const PPMPixel* restrict imageIn, v4Accurate* restrict scratch, v4Accurate* restrict result, const int kernelSize, const int width, const int height) {
-	blurIterationHorizontalFirst(imageIn, scratch, kernelSize, width, height);
-	blurIterationHorizontalAlternative(scratch, result, kernelSize, width, height);
+	// blurIterationHorizontalFirst(imageIn, scratch, kernelSize, width, height);
+	blurIterationHorizontalAlternative(imageIn, result, scratch, kernelSize, width, height);
 	blurIterationHorizontalTranspose(result, scratch, kernelSize, width, height);
 	blurIterationVerticalAlternative(scratch, result, kernelSize, width, height);
 }
